8a886c4a53e52466d9d8efac570a96a187f81de3
Artu5r <144451275+Artu5r@users.noreply.github.com>
2025-02-04 15:23:12 +0100

    Add files via upload

---
+++ 04_02_2025/Zadania/2.js
@@ -0,0 +1,7 @@
+function poleProstokata(a, b){
+    suma = a * b;
+    console.log(suma);
+}
+let a = prompt();
+let b = prompt();
+poleProstokata(a,b);
---
+++ 04_02_2025/Zadania/3.js
@@ -0,0 +1,7 @@
+function czyParzysta(liczba){
+if(liczba % 2 == 0)
+    console.log("Parzysta")
+else
+    console.log("Nieparzysta")
+}
+czyParzysta(prompt())
