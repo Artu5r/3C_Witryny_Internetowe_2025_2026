37d7aeb86ba7f999ef1f781183f87b8aaf4d4293
Artu5r <144451275+Artu5r@users.noreply.github.com>
2025-10-22 08:54:32 +0200

    Add files via upload

--- 22_10_2025/dane.php
+++ 22_10_2025/dane.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles-php.css">
 </head>
 <body>
-    <?php
-        $zmienna1 = $_POST['imie'];
-        echo "Podane imię: " . $zmienna1 . "<br>";
+    <div>
+        <h2>Portfolio</h2>
+        <?php
+            $zmienna1 = $_POST['imie'];
+            echo "" . $zmienna1 . "<br>";
 
-        $zmienna2 = $_POST['nazwisko'];
-        echo "Podane nazwisko: " . $zmienna2 . "<br>";
+            $zmienna2 = $_POST['nazwisko'];
+            echo "" . $zmienna2 . "<br>";
 
-        $zmienna3 = $_POST['data_urodzenia'];
-        echo "Podana data urodzenia: " . $zmienna3 . "<br>";
+            $zmienna3 = $_POST['data_urodzenia'];
+            echo "" . $zmienna3 . "<br>";
 
-        $zmienna4 = $_POST['numer_telefonu'];
-        echo "Podany numer telefonu: " . $zmienna4 . "<br>";
+            $zmienna4 = $_POST['numer_telefonu'];
+            echo "" . $zmienna4 . "<br>";
 
-        $zmienna5 = $_POST['adres_email'];
-        echo "Podany adres email: " . $zmienna5 . "<br>";
+            $zmienna5 = $_POST['adres_email'];
+            echo "" . $zmienna5 . "<br>";
 
-        $zmienna6 = $_POST['adres_zamieszkania'];
-        echo "Podany adres zamieszkania: " . $zmienna6 . "<br>";
+            $zmienna6 = $_POST['adres_zamieszkania'];
+            echo "" . $zmienna6 . "<br>";
 
-        $zmienna7 = $_POST['numer_domu'];
-        echo "Podany numer domu: " . $zmienna7 . "<br>";
+            $zmienna7 = $_POST['numer_domu'];
+            echo "" . $zmienna7 . "<br>";
 
-        $zmienna8 = $_POST['kod_pocztowy'];
-        echo "Podany kod pocztowy: " . $zmienna8 . "<br>";  
-    ?>
+            $zmienna8 = $_POST['kod_pocztowy'];
+            echo "" . $zmienna8 . "<br>";  
+        ?>
+    </div>
 </body>
 </html>
