5cb02b95896432fa3244c3484696e1a2eb18c14e
Artu5r <144451275+Artu5r@users.noreply.github.com>
2026-01-07 08:52:57 +0100

    Add files via upload

--- 07_01_2025/zad2.php
+++ 07_01_2025/zad2.php
@@ -1,17 +1,23 @@
-<?php
-// Generowanie 6 losowych liczb 1-49
-$liczby = [];
-for ($i = 0; $i < 6; $i++) {
-    $liczby[] = rand(1, 49);
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $liczby = [];
+    for ($i = 0; $i < 6; $i++) {
+        $liczby[] = rand(1, 49);
+    }
 
-// Aktualna data
-$data = date("d-m-Y H:i:s");
+    $data = date("d-m-Y H:i:s");
 
-// Zapis do pliku
-$wpis = "$data wylosowano liczby: " . implode(", ", $liczby) . "\n";
-file_put_contents("liczby.txt", $wpis, FILE_APPEND);
+    $wpis = "$data wylosowano liczby: " . implode(", ", $liczby) . "\n";
+    file_put_contents("liczby.txt", $wpis, FILE_APPEND);
 
-// Odczyt pliku
-echo file_get_contents("liczby.txt");
-?>
+    echo file_get_contents("liczby.txt");
+    ?>
+</body>
+</html>
